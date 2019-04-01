92ff468b4aa23dc27bac5e3c9a265f32751c4a19
cryptopotluck <cryptopotluck@gmail.com>
2019-04-01 02:11:58 -0500

    learning react

---
+++ src/components/Contact.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  render() {
+    const { contact } = this.props;
+    const { showContactInfo } = this.state;
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {contact.name}{" "}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+          />
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {contact.email}</li>
+            <li className="list-group-item">Phone: {contact.phone}</li>
+          </ul>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+export default Contact;
